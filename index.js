b8c2dba8930b1864e92be6e07cdfd2196293f389
YehorSavin <yagaro4ek@gmail.com>
2020-09-07 09:56:47 +0300

    kjbut

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 'use strict'
 
-let a;
-console.log(a);
+let Vova = {
+  age: 18,
+}
 
+// if(Vova.age >= 18 || vova.dad === 'deputat') {
+// alert('prohodi brat')
+// } else {
+//   alert('huy sosi');
+// }
+
+
+
+// Vova.age >= 18 || Vova.dad === 'deputat' ? alert('prohodi brat'): alert('huy sosi');
+
+
+
+// const PASSWORD = 'ilovejs';
+
+// let prompt1 = prompt('Enter your name: ');
+
+// if(prompt1) {
+//   prompt1 === PASSWORD 
+//   ? console.log('Youre in')
+//   : console.log('wrong password')
+// } else{
+//   alert('you have to enter the password')
+// }
+
+
+// let q = 1;
+
+// while (q) {
+//   q = prompt('enter your number: ');
+
+//   console.log(q)
+// }
+
+
+// for(let i = 0; i<10; i++) {
+
+//   console.log('i')
+// }
+
+
+// let mine = 20;
+// let neighbor = 28;
+// let main = mine + neighbor;
+
+
+let a = 10;
+a = 20;
+a = a + 20;
